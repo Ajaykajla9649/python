5149ee581e8f2af37952f15c3696f6d7a05c8346
Ajay kajla <ajaykajla9649@gmail.com>
2025-06-01 16:06:43 +0530

    Add files via upload

---
+++ chapter_2/03_rules.py
@@ -0,0 +1,16 @@
+a = 23
+
+aa = 453
+
+ajay = 54
+
+sameer = 96
+
+_ajay = 72
+
+# @ajay = 56
+
+# A variable name can contain alphabets, digits, and underscores.
+# A variable name can only start with an alphabet and underscores.
+# A variable name can't start with a digit.
+# No while space is allowed to be used inside a variable name.
---
+++ chapter_2/04_operators.py
@@ -0,0 +1,21 @@
+# arithmatic operators
+a = 7
+b = 4
+c = a + b
+print(c)
+
+# assignment operators
+a = 4-2 # assign 4-2 is a
+print (a)
+b = 6
+b += 3 #increment the value of b by 3 and assign it to b
+print (b)
+
+#comparision operator
+
+d = 5<4
+print (d)
+
+#logical operators
+e = True or False
+print (e)
